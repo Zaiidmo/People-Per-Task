bcee813fb523f2ee7fb16372fd77bdb5aeb2007c
Zaiid Moumnii <zaiidmoumnii@gmail.com>
2023-12-06 15:19:56 +0100

    Creating dotEnv file

--- app/controllers/mail.php
+++ app/controllers/mail.php
@@ -6,14 +6,17 @@ use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require './mailer/autoload.php';
+require './vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 try {
 $mail = new PHPMailer();
 
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    $mail->Username   = 'zaiidmoumnii@gmail.com';
-    $mail->Password   = 'kbfk dewe qbqm bkot';
+    $mail->Username   = $_ENV['MAIL'];
+    $mail->Password   = $_ENV['MAILER_PASSWORD'];
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;     
     
